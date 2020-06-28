c2ab65bdb3d2c3a0b582d1a4a18a5a126a9a69d1
Girija Viswanathan <girijavmohan@gmail.com>
2020-06-28 16:42:23 -0700

    partial product

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -12,8 +12,9 @@ const reducer = (oldstate, action) => {
 //Set the active record            
         case "ACTIVE_REC":
             let oldRec = JSON.parse(JSON.stringify(oldstate.records[parseInt(action.recordId)]))
-
-            return {...oldstate,recordId:action.recordId,oldRecord:oldRec,edit:!oldstate.edit,activeRecord:oldstate.records[parseInt(action.recordId)]}
+            let editingrec = JSON.parse(JSON.stringify(oldstate.records[parseInt(action.recordId)]))
+            return {...oldstate,recordId:action.recordId,oldRecord:oldRec,edit:!oldstate.edit,activeRecord:oldstate.records[parseInt(action.recordId)],edittingrec:editingrec}
+            // return {...oldstate,recordId:action.recordId,oldRecord:oldRec,edit:!oldstate.edit,activeRecord:oldstate.records[parseInt(action.recordId)]}
             // return {...oldstate,recordId:action.recordId,oldRecord:oldRec,edit:!oldstate.edit,activeRecord:oldRec}
 
 //Reset the active record to its initial value
@@ -22,28 +23,41 @@ const reducer = (oldstate, action) => {
 
 //Holds the intermediate changes made to the record
         case "CANCEL_RECORD":
-            debugger
+            // debugger
             let rec = oldstate.oldRecord
             let recs = oldstate.records.map((rec,id) => {
                 if(id === parseInt(action.idx)){
                     // debugger
-                    rec = oldstate.oldRecord
+                    rec = JSON.parse(JSON.stringify(oldstate.oldRecord))
                 }
                 return rec
             })
-            // debugger
-            return {...oldstate,records:recs,activeRecord:rec,oldRecord:{}}
+            let oldRec1 = JSON.parse(JSON.stringify(oldstate.records[parseInt(action.recordId)]))
+            let editingrec1 = JSON.parse(JSON.stringify(oldstate.records[parseInt(action.recordId)]))
+            return {...oldstate,records:recs,activeRecord:rec,oldRecord:oldRec1,edittingrec:editingrec1}
 
 //Update a record
+        // case "UPDATE_RECORD":
+        //     let newRecs = oldstate.records.map((rec,id) => {
+        //         if(id === parseInt(action.idx)){
+        //             // debugger
+        //             rec[action.fieldname] = action.value 
+        //         }
+        //         return rec
+        //     })
+        //     return {...oldstate,records:newRecs}
         case "UPDATE_RECORD":
             let newRecs = oldstate.records.map((rec,id) => {
                 if(id === parseInt(action.idx)){
                     // debugger
-                    rec[action.fieldname] = action.value 
+                    for(const key in (action.record)){
+                        rec[key] = action.record[key]
+                    }
                 }
                 return rec
             })
-            return {...oldstate,records:newRecs}
+            // debugger
+            return {...oldstate,records:newRecs,activeRecord:JSON.parse(JSON.stringify(oldstate.records[parseInt(action.recordId)]))}
 
 //Default Action
         default:
--- src/redux/store.js
+++ src/redux/store.js
@@ -7,6 +7,8 @@ const initialState = {records:[],recordId:'',oldRecord:{},activeRecord:{},editti
 //                     "email":"parthasarathy@parthasarathy.parthasarathy","password":"parthasarathy123","address1":"parthasarathy",
 //                     "state":"parthasarathy","city":"parthasarathy","zipcode":"parthasarathy","country":"parthasarathy"}],recordId:'0',activeRecord:{"firstname":"parthasarathy","lastname":"perumal","username":"parthasarathy",
 //                     "email":"parthasarathy@parthasarathy.parthasarathy","password":"parthasarathy123","address1":"parthasarathy",
+//                     "state":"parthasarathy","city":"parthasarathy","zipcode":"parthasarathy","country":"parthasarathy"},edittingrec:{"firstname":"parthasarathy","lastname":"perumal","username":"parthasarathy",
+//                     "email":"parthasarathy@parthasarathy.parthasarathy","password":"parthasarathy123","address1":"parthasarathy",
 //                     "state":"parthasarathy","city":"parthasarathy","zipcode":"parthasarathy","country":"parthasarathy"}}
 
 //Store creation
